10b834a31a9ac3ee5b50ea9431326c67be8f6607
ToBeginToPonderOver <121504896+ToBeginToPonderOver@users.noreply.github.com>
2024-01-11 10:42:26 +0530

    some correction

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let string = "";
 let buttons = document.querySelectorAll('.button')
 
+
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
         if (e.target.innerHTML === '=') {
@@ -18,10 +19,36 @@ buttons.forEach((button) => {
         else if (e.target.innerHTML === '+ | -') {
             string = -1 * parseFloat(string);
             document.querySelector('input').value = string;
-        } 
-        else if (e.target.innerHTML !== '') {
+        }
+        else if (e.target.innerHTML === '1' || e.target.innerHTML === '2' || e.target.innerHTML === '3' || e.target.innerHTML === '4' || e.target.innerHTML === '5' || e.target.innerHTML === '6' || e.target.innerHTML === '7' || e.target.innerHTML === '8' || e.target.innerHTML === '9' || e.target.innerHTML === '0' || e.target.innerHTML === '.') {
             string = string + e.target.innerHTML;
             document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML === '+' || e.target.innerHTML === '-' || e.target.innerHTML === '*' || e.target.innerHTML === '/') {
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML ==='%'){
+            let parts1 = string.split('+');
+            let parts2 = string.split('-');
+            let parts3 = string.split('*');
+            let parts4 = string.split('/');
+            if (!isNaN(parts[parts.length - 1])) {
+                if (string = parts.join('+')){
+                    document.querySelector('input').value = parseFloat(parts1[0]) + parseFloat(parts1[0]*(parts1[parts1.length-1]/100));
+                }
+                else if (string=parts.join('-')){
+                    document.querySelector('input').value = parseFloat(parts[0]) - parseFloat(parts[0]*(parts[parts.length-1]/100));
+                }
+                else if (string=parts.join('*')){
+                    document.querySelector('input').value = parseFloat(parts[0]) * parseFloat(parts[0]*(parts[parts.length-1]/100));
+                }
+                else if (string=parts.join('/')){
+                    document.querySelector('input').value = parseFloat(parts[0]) / parseFloat(parts[0]*(parts[parts.length-1]/100));
+                }
+            } else {
+                console.error('Error: string is not a number');
+            }
         }
     });
 });
